d85cdc68089ad6119b3d25ab47672c3ac0ce43f8
Diego <egoroof1@gmail.com>
2025-08-24 13:51:34 +0300

    finish(2.0)

--- app/src/main/java/com/diego/marathonapp/EgorovActivity.kt
+++ app/src/main/java/com/diego/marathonapp/EgorovActivity.kt
@@ -1,12 +1,17 @@
 package com.diego.marathonapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class EgorovActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,16 @@ class EgorovActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        findViewById<ImageView>(R.id.btn_back).setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainClose", "MainClose")
     }
 }
--- app/src/main/java/com/diego/marathonapp/MainActivity.kt
+++ app/src/main/java/com/diego/marathonapp/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.diego.marathonapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +19,10 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        findViewById<MaterialButton>(R.id.btn_egorov).setOnClickListener {
+            startActivity(Intent(this, EgorovActivity::class.java))
         }
     }
 }
